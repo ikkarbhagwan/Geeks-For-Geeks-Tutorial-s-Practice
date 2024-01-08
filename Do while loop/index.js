14e2313cbb1861f0a41e4a05cf7c9eb0d60dadf0
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-08 23:32:16 +0530

    do while

---
+++ Do while loop/index.js
@@ -0,0 +1,9 @@
+
+
+let text = "";
+let i = 0;
+do {
+  text += i + "<br>";
+  i++;
+}
+while (i < 5);
--- nested Loop/nested Loop.js
+++ nested Loop/nested Loop.js
@@ -1,58 +1,58 @@
-// // for (let i = 1; i < 10; i++) {
+for (let i = 1; i < 10; i++) {
 
-// //     for (let j = 1; j < 12; j++) {
+    for (let j = 1; j < 12; j++) {
 
-// //         let products = i * j;
-// //         console.log(`${i} * ${j} = ${products} `);
-// //     }
+        let products = i * j;
+        console.log(`${i} * ${j} = ${products} `);
+    }
 
-// //  for of loop
+//  for of loop
 
-// const Fname = "Bhgawan Ikkar";
-// const stringLength = Fname.length;
+const Fname = "Bhgawan Ikkar";
+const stringLength = Fname.length;
 
-// for (let i = 0; i < stringLength; i++) {
-//     console.log(Fname[i]);
-// }
+for (let i = 0; i < stringLength; i++) {
+    console.log(Fname[i]);
+}
 
-// //  making start pattern
-// let symbol = "* ";
+//  making start pattern
+let symbol = "* ";
 
-// for (let i = 0; i <= 8; i++) {
-//     console.log(symbol.repeat(i));
-// }
+for (let i = 0; i <= 8; i++) {
+    console.log(symbol.repeat(i));
+}
 
-// let symbolN = "* ";
+let symbolN = "* ";
 
-// for (let i = 0; i <= 8; i++) {
-//     console.log(symbolN.repeat(i));
-// }
+for (let i = 0; i <= 8; i++) {
+    console.log(symbolN.repeat(i));
+}
 
-// // reverse
+// reverse
 
-// let start = "* ";
+let start = "* ";
 
-// for (let i = 6; i <= 6; i--) {
-//     console.log(start.repeat(i));
+for (let i = 6; i <= 6; i--) {
+    console.log(start.repeat(i));
 
-// }
+}
 
-// // find the no. of char
-// let myName = "Bhagwan Govindrao Ikkar";
-// let count = 0;
+// find the no. of char
+let myName = "Bhagwan Govindrao Ikkar";
+let count = 0;
 
-// for (let i = 0; i < myName.length; i++) {
-//     count++;
-// }
-// console.log(`The number of characters in the string is ${count}`);
+for (let i = 0; i < myName.length; i++) {
+    count++;
+}
+console.log(`The number of characters in the string is ${count}`);
 
-// // using length
-// let myName1 = "Bhagwan Govindrao Ikkar";
-// let count1 = myName1.length;
+// using length
+let myName1 = "Bhagwan Govindrao Ikkar";
+let count1 = myName1.length;
 
-// console.log(`The number of characters in the string is ${count1}`);
+console.log(`The number of characters in the string is ${count1}`);
 
-///find all the even No. from this
+// /find all the even No. from this
 
 
 let reminder = 0;
@@ -61,9 +61,20 @@ for (i = 0; i <= 100; i++) {
 
     if (reminder = i % 2) {
         console.log(`${i} is an EVen Number`);
-    }else{
+    } else {
         let odd = `${i} is an odd number`;
         console.log(odd);
-        
+
+    }
+}
+
+
+//  cheaking is Vovels 
+let stringName = 'This is my space!';
+let vowels = "aeiou";
+
+for (let i = 0; i < stringName.length; i++) {
+    if (vowels.includes(stringName[i])) {
+        console.log(`${stringName[i]} is a vowel`);
     }
 }
