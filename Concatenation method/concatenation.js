d0a6006f539f9c4b87e1b9ac93d5d2c2ffea5081
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-08 13:39:22 +0530

    concatenation

---
+++ Concatenation method/concatenation.js
@@ -0,0 +1,8 @@
+let myName = "Bhagwan";
+let surName = "ikkar";
+
+let fullName = "my name is "+ myName + " and my surname is "+ surName;
+
+console.log(fullName);
+
+//this is concatenation method 
