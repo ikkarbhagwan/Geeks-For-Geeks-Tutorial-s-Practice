26171b619a939a4739ff6ce32da485b8b4b0d1ee
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-08 22:52:26 +0530

    find the no. of char
    find the no. of char

--- nested Loop/nested Loop.js
+++ nested Loop/nested Loop.js
@@ -13,6 +13,46 @@
 const Fname = "Bhgawan Ikkar";
 const stringLength = Fname.length;
 
-for(let i = 0; i<stringLength; i++){
+for (let i = 0; i < stringLength; i++) {
     console.log(Fname[i]);
 }
+
+//  making start pattern 
+let symbol = "* ";
+
+for (let i = 0; i <= 8; i++) {
+    console.log(symbol.repeat(i));
+}
+
+
+let symbolN = "* ";
+
+for (let i = 0; i <= 8; i++) {
+    console.log(symbolN.repeat(i));
+}
+
+// reverse 
+
+let start = "* ";
+
+for (let i = 6; i <= 6; i--) {
+    console.log(start.repeat(i));
+
+}
+
+// find the no. of char 
+let myName = "Bhagwan Govindrao Ikkar";
+let count = 0;
+
+for (let i = 0; i <myName.length; i++) {
+    count++;
+}
+console.log(`The number of characters in the string is ${count}`);
+
+
+
+// using length 
+let myName1 = "Bhagwan Govindrao Ikkar";
+let count1 = myName1.length;
+
+console.log(`The number of characters in the string is ${count1}`);
