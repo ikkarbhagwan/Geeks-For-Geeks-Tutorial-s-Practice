94eea7b085b728d353cf9339a2b11aa3a63a124d
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-09 00:25:31 +0530

    calculateSum

---
+++ function exercise/index.js
@@ -0,0 +1,18 @@
+
+
+function calculateSum(min, max) {
+    let total = 0;
+
+
+    for (let i = min; i <= max; i++) {
+
+        total = total + i;
+
+    }
+    console.log(total);
+
+
+}
+const result = calculateSum(1,20);
+// console.log(result);
+
