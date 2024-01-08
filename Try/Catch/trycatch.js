67441a2de983a7707b81df14a38f5fe3e075962e
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-09 00:17:48 +0530

    writing code

--- Try/Catch/trycatch.js
+++ Try/Catch/trycatch.js
@@ -2,13 +2,29 @@
 
 const userName = "Bhagwan";
 
-try{
+try {
     console.log("this is")
 
-}catch(errror){
+} catch (errror) {
     console.log(error.name);
 
 }
 
 
 console.log(userName);
+
+//  enter a number that is less than or equal to 50
+
+let userNumber;
+
+do {
+    userNumber = prompt("Enter a number:");
+
+    if (userNumber > 50) {
+        alert("Please enter a number less than or equal to 50.");
+    }
+} while (userNumber > 50);
+
+alert("Thank you! You entered a number less than or equal to 50.");
+
+
