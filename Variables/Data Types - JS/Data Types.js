4f157e704cddfcb731f2958f9ef295c7c8cb6220
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-08 13:32:20 +0530

    data types

---
+++ Variables/Data Types - JS/Data Types.js
@@ -0,0 +1,17 @@
+let x = 10;
+console.log(typeof x); // Outputs: "number"
+
+let y = "Hello, World!";
+console.log(typeof y); // Outputs: "string"
+
+let z = true;
+console.log(typeof z); // Outputs: "boolean"
+
+
+
+
+// Undefined: Represents the absence of a value or an uninitialized variable.
+// Null: Represents the intentional absence of any object value.
+// Boolean: Represents a logical value, either true or false.
+// Number: Represents numeric values, including integers and floating-point numbers.
+// String: Represents a sequence of characters enclosed in single or double quotes.
