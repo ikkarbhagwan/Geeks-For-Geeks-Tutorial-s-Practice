544bfce667f161e0fa6efefa6d58753f5f4c425d
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-09 20:38:46 +0530

    // in this exmaple, the firstname and lasname properties are assinged to fanem and lname variable srespectively

--- Destructring objects/index.js
+++ Destructring objects/index.js
@@ -1,16 +1,88 @@
 let obj = {
-    name : "bhagwan",
+    name: "bhagwan",
     age: 23,
     city: "pune",
-    adress:{
-        area:"nagar",
-        pin:431424,
+    adress: {
+        area: "nagar",
+        pin: 431424,
     },
 
-    courses :['html','css','javascript','react']
+    courses: ['html', 'css', 'javascript', 'react']
 }
 
-let {name,age,...rest} = obj;
+let { name, age, ...rest } = obj;
 
 console.log(name);
 console.log(rest);
+
+//  nested objects 
+
+let employeee = {
+
+    engineers: {
+
+        emp2: {
+            game: "mumbai",
+            age: 45,
+            city: "selu"
+
+
+
+
+
+        },
+
+        emp5: {
+            game: "arjun",
+            age: 45,
+            city: "parbhani"
+
+
+
+
+
+        }
+
+
+
+
+
+
+    },
+
+    youtube: {
+
+        emp3: {
+
+            game: "suresh",
+            age: 45,
+            city: "delhi"
+
+
+
+
+        },
+        emp4: {
+
+            game: "damu",
+            age: 45,
+            city: "mombai"
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
+
+
+let {engineers:{game}} = engineers;
+console.log(game);
