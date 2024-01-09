a400e306863e6b558ca65e69c8858634a9950cce
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-09 19:43:05 +0530

    optional chaining

--- optional chaning/index.js
+++ optional chaning/index.js
@@ -1,21 +1,32 @@
 
 
-const obj1= {
-    name:"a",
-    age:23,
-    city:"pune",
- adress:{
-     area:"ab-1",
-          turn:2
+const obj1 = {
+    name: "a",
+    age: 23,
+    city: "pune",
+    adress: {
+        area: "ab-1",
+        turn: 2
 
- },
+    },
+    getCityName : function (){
+        console.log("welcome bhagwan!");
+
+    }
 }
 
 console.log(obj1.adress.area);
 
-if(obj1.adress !== undefined){
+if (obj1.adress !== undefined) {
     console.log(obj1.adress.area);
 
-}else{
+} else {
     console.log("adress not found");
 }
+obj1.getCityName();
+console.log(obj1.userMsg?.());
+
+
+
+// optional chaining 
+
