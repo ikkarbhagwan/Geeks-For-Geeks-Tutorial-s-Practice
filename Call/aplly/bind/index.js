eda2fcc28d0d69b1bc8a8e55efcd48d12418e59f
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-10 00:59:59 +0530

    Update index.js

--- Call/aplly/bind/index.js
+++ Call/aplly/bind/index.js
@@ -24,3 +24,35 @@ let name2 = {
 }
 
 printFullName.call(name2, "mumbai");
+
+
+
+// 2. 
+
+let myName = {
+
+    fName: "bhagwan",
+    lName: "ikkar",
+
+}
+let logFullName = function(discrit,game){
+    console.log(this.fName+" " + this.lName +" " + discrit + game);
+}
+
+logFullName.call(myName, "pune","hollyabaoll");
+
+let hisName = {
+
+    fName:"ramesh",
+    lName:"sharma",
+
+}
+// call method 
+
+logFullName.call(hisName, "selu","cricket");
+// applay 
+logFullName.apply(hisName, ["cricket", "lolo"]);
+
+
+
+
