ed2c9c8dfd939d99b5dcc0e1dd1e9b2bbb963434
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-10 11:23:45 +0530

    Update index.js

--- Call/aplly/bind/index.js
+++ Call/aplly/bind/index.js
@@ -1,58 +1,90 @@
 
-let name = {
+// let name = {
 
-    firstName: "bhagwan",
-    lastname: "ikkar",
+//     firstName: "bhagwan",
+//     lastname: "ikkar",
 
+// }
+
+// printFullName = function (hometown) {
+//     console.log(this.firstName + " " + this.lastname + " from " + hometown);
+
+// }
+
+
+// printFullName.call(name, "dehradun");
+
+
+
+// let name2 = {
+
+//     firstName: 'krishna',
+//     lastname: "ikkar",
+
+// }
+
+// printFullName.call(name2, "mumbai");
+
+
+
+// // 2. 
+
+// let myName = {
+
+//     fName: "bhagwan",
+//     lName: "ikkar",
+
+// }
+// let logFullName = function(discrit,game){
+//     console.log(this.fName+" " + this.lName +" " + discrit + game);
+// }
+
+// logFullName.call(myName, "pune","hollyabaoll");
+
+// let hisName = {
+
+//     fName:"ramesh",
+//     lName:"sharma",
+
+// }
+// // call method 
+
+// logFullName.call(hisName, "selu","cricket");
+// // applay 
+// logFullName.apply(hisName, ["cricket", "lolo"]);
+
+
+// 3. 
+
+let person = {
+    firstName: "shree ram",
+    lastname:"ikkar",
+  
+}
+let printFullName= function(discrict){
+    console.log(this.firstName+" " + this.lastname+ " "+discrict)
 }
 
-printFullName = function (hometown) {
-    console.log(this.firstName + " " + this.lastname + " from " + hometown);
+printFullName.call(person, "parbhani");
+
+
+let person2 = {
+    firstName: "lakhan",
+    lastname:"yadav",
 
 }
+// function borrowing 
+
+printFullName.call(person2, "selu");
+printFullName.apply(person2, ["parbhani" ,"selu"])
 
 
-printFullName.call(name, "dehradun");
+// 
+let myNumber = "9515904844";
 
+let sumOfNum = Number(myNumber);
 
+console.log(sumOfNum);
 
-let name2 = {
-
-    firstName: 'krishna',
-    lastname: "ikkar",
-
-}
-
-printFullName.call(name2, "mumbai");
-
-
-
-// 2. 
-
-let myName = {
-
-    fName: "bhagwan",
-    lName: "ikkar",
-
-}
-let logFullName = function(discrit,game){
-    console.log(this.fName+" " + this.lName +" " + discrit + game);
-}
-
-logFullName.call(myName, "pune","hollyabaoll");
-
-let hisName = {
-
-    fName:"ramesh",
-    lName:"sharma",
-
-}
-// call method 
-
-logFullName.call(hisName, "selu","cricket");
-// applay 
-logFullName.apply(hisName, ["cricket", "lolo"]);
-
-
-
+let sum = sumOfNum
 
