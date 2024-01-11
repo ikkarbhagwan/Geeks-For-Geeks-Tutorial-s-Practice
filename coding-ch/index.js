12f038ab63bb463563a80cd83532ee1de108ea5f
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-11 23:55:07 +0530

    code updated

--- coding-ch/index.js
+++ coding-ch/index.js
@@ -1,26 +1,92 @@
-const string = ["this is my house make it beautiful.."];
+// const string = ["this is my house make it beautiful.."];
 
-function getStrNoVowels(str) {
-    let result = "";
+// function getStrNoVowels(str) {
+//     let result = "";
 
-    for (let char of str[0]) { 
-        let vowels = ["a", "e", "i", "o", "u"];
-        if (!vowels.includes(char.toLowerCase())) {
-            result += char;
-        }
+//     for (let char of str[0]) { 
+//         let vowels = ["a", "e", "i", "o", "u"];
+//         if (!vowels.includes(char.toLowerCase())) {
+//             result += char;
+//         }
+//     }
+
+//     return result;
+// }
+
+// const updateStr = getStrNoVowels(string);
+// console.log(updateStr);
+
+// // 2. 
+
+// let str = "125487846543";
+
+// const maskChar = "#".repeat(4);
+// const resultedNum = str.slice(0, str.length - 4) + maskChar;
+
+// console.log(resultedNum);
+
+
+// // 3.
+
+// const inputStr = "Bhagwan";
+
+// let aToz = "abcdefghijklmnopqrstuvwxyz";
+// let updated ="";
+
+
+
+// for(let i = 0; i>inputStr.length.toLowerCase(inputStr); i++){
+//     if(inputStr[i] =[i]){
+//         aToz =i+2;
+//         updated = aToz[i]
+
+//     }
+//     return updated;
+// }
+
+// 4. 
+
+const arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const outputResult = arrNum.map(num => num % 2 === 0 ? num - 1 : num + 1);
+console.log(outputResult);
+
+
+// 5. sum of Num which is less than 40
+
+let givenData = [12, 45, 87, 10, 23, 99];
+
+let findSum = givenData.reduce((acc, curr) => curr < 40 ? acc + curr : acc, 0);
+console.log(findSum);
+
+
+// 5 .
+
+const obj = [
+    {
+        name: "ikkar",
+        age : 35,
+    },
+    {
+        name: "kadam",
+        age : 23,
+    },
+    {
+        name: "kitale",
+        age : 56,
     }
+]
 
-    return result;
-}
 
-const updateStr = getStrNoVowels(string);
-console.log(updateStr);
+// find only names 
 
-// 2. 
 
-let str = "125487846543";
+const findAns = obj.filter(obj => obj.age>30).map(obj => obj.name);
+console.log(findAns);
 
-const maskChar = "#".repeat(4);
-const resultedNum = str.slice(0, str.length - 4) + maskChar;
 
-console.log(resultedNum);
+//  sum of even and odd num 
+
+let getData = [1,2,3,4,5,6,7];
+
+let goodOutput = getData.reduce
