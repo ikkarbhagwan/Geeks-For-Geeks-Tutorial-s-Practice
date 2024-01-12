c318aced1788219d47b8e5f4693e9e2947013fcf
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-13 01:29:42 +0530

    c

--- Throttling/index.js
+++ Throttling/index.js
@@ -1,6 +1,14 @@
 
 const container = document.querySelector('.container');
 
+const throttle =(callback , delay) => {
+    isWaiting = false;
+    return () => {
+        callback();
+    }
+}
+
+
 const handleScorlle = () =>{
     console.log("scrolled");
 };
