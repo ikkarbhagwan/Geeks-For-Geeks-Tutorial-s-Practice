8b752851935eae6c6a3add87945b8c996075902f
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-13 23:48:35 +0530

    update

--- Call/aplly/bind/index.js
+++ Call/aplly/bind/index.js
@@ -88,3 +88,65 @@ console.log(sumOfNum);
 
 let sum = sumOfNum
 
+// //////////
+// call  method 
+
+let name = {
+firstName : "bhagwan",
+lastname : "ikkar",
+
+}
+let getFullname = function (town , city){
+
+    console.log(this.firstName + " " + this.lastname
+     + "from" + town );
+
+}
+
+getFullname.call(name, "parbhani" , "selu");
+
+
+// this called as function bowwring 
+
+
+let name2 = {
+
+    firstName : "abhay",
+    lastname : "bongane",
+
+}
+
+getFullname.call(name2, "jinutr", "mumbai");
+
+
+// 2. 
+
+let teacher = {
+
+    period :"Maths",
+    duration : 2.5,
+
+
+}
+
+let findSub = function (start,End){
+
+    console.log("teaching will starts of " + 
+    this.duration + " " + " of " + this.period);
+
+}
+
+findSub.call(teacher, "9:30", "11:30");
+
+
+let teacher2 = {
+
+    period:"science",
+    duration:1.5,
+
+}
+
+findSub.call(teacher2, "11:30" , "12:30");
+findSub.apply(teacher2, ["11:30" , "12:30"]);
+
+// this is examples of call method 
