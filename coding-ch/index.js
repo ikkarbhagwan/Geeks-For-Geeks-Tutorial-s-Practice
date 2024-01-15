d30f838e36920c1a34fc8e77b6cc284633bd03e6
Bhagwan_Ikkar <118511668+ikkarbhagwan@users.noreply.github.com>
2024-01-15 21:00:41 +0530

    use new set for fining Unique values

--- coding-ch/index.js
+++ coding-ch/index.js
@@ -1,5 +1,7 @@
 // const string = ["this is my house make it beautiful.."];
 
+import { number } from "prop-types";
+
 // function getStrNoVowels(str) {
 //     let result = "";
 
@@ -46,98 +48,109 @@
 
 // 4. 
 
-const arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const arrNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const outputResult = arrNum.map(num => num % 2 === 0 ? num - 1 : num + 1);
-console.log(outputResult);
-
-
-// 5. sum of Num which is less than 40
-
-let givenData = [12, 45, 87, 10, 23, 99];
-
-let findSum = givenData.reduce((acc, curr) => curr < 40 ? acc + curr : acc, 0);
-console.log(findSum);
+// const outputResult = arrNum.map(num => num % 2 === 0 ? num - 1 : num + 1);
+// console.log(outputResult);
 
 
-// 5 .
+// // 5. sum of Num which is less than 40
 
-const obj5 = [
-    {
-        name: "ikkar",
-        age : 35,
-    },
-    {
-        name: "kadam",
-        age : 23,
-    },
-    {
-        name: "kitale",
-        age : 56,
-    }
-]
+// let givenData = [12, 45, 87, 10, 23, 99];
+
+// let findSum = givenData.reduce((acc, curr) => curr < 40 ? acc + curr : acc, 0);
+// console.log(findSum);
 
 
-// find only names 
+// // 5 .
+
+// const obj5 = [
+//     {
+//         name: "ikkar",
+//         age : 35,
+//     },
+//     {
+//         name: "kadam",
+//         age : 23,
+//     },
+//     {
+//         name: "kitale",
+//         age : 56,
+//     }
+// ]
 
 
-const findAns = obj.filter(obj => obj.age>30).map(obj => obj.name);
-console.log(findAns);
+// // find only names 
 
 
-//  sum of even and odd num 
-
-let getData = [1,2,3,4,5,6,7];
-
-let goodOutput = getData.reduce
+// const findAns = obj.filter(obj => obj.age>30).map(obj => obj.name);
+// console.log(findAns);
 
 
+// //  sum of even and odd num 
 
-let name = "this is Bhagwan";
+// let getData = [1,2,3,4,5,6,7];
 
-let withoutSpacesString = name.replace(/ /g, "");
-console.log(withoutSpacesString);
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
-let a = "this is row string";
-let ans = a.replace(/ /g, "");
-console.log(ans);
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
- let string = "this is row string";
-
- let stringb = string.split(' ').join('');
- console.log(stringb);
-
- //////////////////////////////////////////////////////////////////////////////////////////////
+// let goodOutput = getData.reduce
 
 
 
- let bat = "this is row room";
+// let name = "this is Bhagwan";
 
- let batAns = bat.split(' ').filter( (char) => { char !== " " }).join('');
+// let withoutSpacesString = name.replace(/ /g, "");
+// console.log(withoutSpacesString);
 
- console.log(batAns);
+// ///////////////////////////////////////////////////////////////////////////////////////////////
+
+// let a = "this is row string";
+// let ans = a.replace(/ /g, "");
+// console.log(ans);
+
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////
+
+//  let string = "this is row string";
+
+//  let stringb = string.split(' ').join('');
+//  console.log(stringb);
+
+//  //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//  let bat = "this is row room";
+
+//  let batAns = bat.split(' ').filter( (char) => { char !== " " }).join('');
+
+//  console.log(batAns);
+
+
+// //////////////////////////////////////////////////////////////////////////////////////////////
+  
+// let obj = { name: "Prakash", age: 111, hairColor: "black", likes: "food" };
+// let invertedObject = invertObject(obj);
+
+// function invertObject(obj) {
+//     let invertedObj = {};
+  
+//     for (let key in obj) {
+//       invertedObj[obj[key]] = key;
+//     }
+  
+//     return invertedObj;
+//   }
+
+  
+//   console.log(invertedObject);
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////
-  
-let obj = { name: "Prakash", age: 111, hairColor: "black", likes: "food" };
-let invertedObject = invertObject(obj);
 
-function invertObject(obj) {
-    let invertedObj = {};
-  
-    for (let key in obj) {
-      invertedObj[obj[key]] = key;
-    }
-  
-    return invertedObj;
-  }
+const numbers = [1, 2, 3, 4, 5, 4, 3, 4, 3, 2, 1, 6, 7, 5];
 
-  
-  console.log(invertedObject);
+const uniqueNumbers = new Set(numbers);
+
+console.log(uniqueNumbers);
+
+
   
